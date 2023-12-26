5b09baa7f41c4d88a392b1d1fdd0a297c8a83621
Sixela963 <sixela963@gmail.com>
2023-12-26 02:36:18 +0100

    Initial commit, final (?) base setup cleanup

--- src/main/java/fr/sixela/mechawalkers/MechaWalkersMod.java
+++ src/main/java/fr/sixela/mechawalkers/MechaWalkersMod.java
@@ -34,7 +34,7 @@ import org.slf4j.Logger;
 public class MechaWalkersMod
 {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "MechaWalkers";
+    public static final String MODID = "mecha_walkers";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
