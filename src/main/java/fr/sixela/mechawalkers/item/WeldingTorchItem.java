6c0a3feaca19dbbdb8724864d70464065fa2ee64
Sixela963 <sixela963@gmail.com>
2023-12-26 03:58:49 +0100

    Added basic welding torch item, english language file, and creative tab

---
+++ src/main/java/fr/sixela/mechawalkers/item/WeldingTorchItem.java
@@ -0,0 +1,9 @@
+package fr.sixela.mechawalkers.item;
+
+import net.minecraft.world.item.Item;
+
+public class WeldingTorchItem extends Item {
+    public WeldingTorchItem(Properties pProperties) {
+        super(pProperties);
+    }
+}
