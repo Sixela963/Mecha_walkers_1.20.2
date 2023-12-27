f289f35caccade431e77e62314884ddab9f4ec11
Sixela963 <sixela963@gmail.com>
2023-12-27 19:28:24 +0100

    fixed carrier golem orientation on spawn

--- src/main/java/fr/sixela/mechawalkers/event/ForgeEventBusEvents.java
+++ src/main/java/fr/sixela/mechawalkers/event/ForgeEventBusEvents.java
@@ -7,6 +7,8 @@ import fr.sixela.mechawalkers.entity.MechaWalkersEntities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.FurnaceBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -22,10 +24,14 @@ public class ForgeEventBusEvents {
         if (level.getBlockState(blockPos).is(MechaWalkersBlocks.MECH_CONTROL_SEAT_BLOCK.get()) && level.getBlockState(blockPos.below()).is(Blocks.FURNACE)) {
             CarrierGolem golem = MechaWalkersEntities.CARRIER_GOLEM.get().create(event.getEntity().level());
 
+            float spawnAngle = level.getBlockState(blockPos.below()).getValue(FurnaceBlock.FACING).toYRot();
+
             level.setBlock(blockPos, Blocks.AIR.defaultBlockState(), 2);
             level.setBlock(blockPos.below(), Blocks.AIR.defaultBlockState(), 2);
 
-            golem.moveTo(blockPos.below(),0f,0f);
+            golem.moveTo(blockPos.below(),spawnAngle,0f);
+            golem.setYBodyRot(spawnAngle);
+            golem.setYHeadRot(spawnAngle);
             level.addFreshEntity(golem);
 
             level.blockUpdated(blockPos, Blocks.AIR);
